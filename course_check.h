dce4118e056b4142f852e47c43c6ee047eac3ecf
wxg <1132277123@qq.com>
2015-10-31 23:46:47 +0800

    a main headfile

---
+++ course_check.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Filename      : course_check.h
+ * Author        : Xingguang wei
+ * Email         : jxweixingguang@163.com
+ * Create Date   : 2015-10-31
+ * Last modified : 2015-10-31 19:24
+ * Description   : 
+ * ********************************************************************/
+
+
+typedef struct Student{
+	char * ID,
+	char * name,
+	char * password,
+	int age,
+	char gender,
+	int grade,
+	char inlab} STU;
+
+type struct Teacher{
+	char * ID,
+	char * name,
+	char * password,
+	char gender,
+	char * title,
+	char * research_area,
+	char inlab} TCH;
